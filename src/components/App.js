3e2be191f3077e22c8b1716a923ff348f5fec63c
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-02-22 22:01:05 +0300

    html and css

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
+import logo from '../images/logo.svg'
+
 function App() {
   return (
     <div>
       <header className="header page__conteiner">
         <img
           className="header__logo"
-          src="<%=require('./images/logo.svg')%>"
+          src={logo}
           alt="Логотип Место"
         />
       </header>

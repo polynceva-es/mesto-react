6df867fd01aa5ca89af81b33d9f03d9c26171cf8
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-02-23 13:26:38 +0300

    open popup

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,34 @@
+function PopupWithForm(props) {
+    return(
+        <div className={`popup popup_type_form popup_type_form-${props.name}`}>
+        <div className="popup__conteiner popup__conteiner_form">
+          <button
+            className="button button_type_close"
+            type="button"
+            aria-label="Закрыть"
+          ></button>
+          <h2 className="popup__title">{props.title}</h2>
+          <form
+            className="popup__form popup__form_edit"
+            name={props.name}
+            action="#"
+            method="get"
+            noValidate
+          >
+            {props.children}
+            <button
+              className="button button_type_submit"
+              type="submit"
+              name="saveProfileInfo"
+              aria-label={props.labelSubmit}
+            >
+              {props.labelSubmit}
+            </button>
+          </form>
+        </div>
+      </div>
+
+    )
+}
+
+export default PopupWithForm;

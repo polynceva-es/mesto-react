c89a73a95aaadb5e7dfcaf1fa39970c170fffaae
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-02-23 12:39:42 +0300

    added opened popups

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,32 @@
+function handleEditAvatarClick(){
+  console.log('click edit avatar')
+  document.querySelector('.popup_type_form-editavatar').classList.add('popup_opened');
+}
+
+function handleEditProfileClick() {
+  console.log('click edit profile')
+  document.querySelector('.popup_type_form-editprofile').classList.add('popup_opened');
+}
+
+function handleAddPlaceClick() {
+  console.log('click add card')
+  document.querySelector('.popup_type_form-addcard').classList.add('popup_opened');
+}
+
+
 function Main() {
     return(
       <main className="content page__conteiner">
         <section className="profile">
           <div className="profile__conteiner">
-            <a className="profile__img-link" href="#">
+            <a onClick={handleEditAvatarClick} className="profile__img-link" href="#">
               <img className="profile__img" src="#" alt="Фото профиля" />
             </a>
             <div className="profile__info">
               <div className="profile__info-conteiner">
                 <h1 className="profile__title"></h1>
                 <button
+                  onClick={handleEditProfileClick}
                   className="button button_type_edit"
                   type="button"
                   aria-label="Редактировать профиль"
@@ -19,6 +36,7 @@ function Main() {
             </div>
           </div>
           <button
+            onClick={handleAddPlaceClick}
             className="button button_type_add"
             type="button"
             aria-label="Добавить карточку"
